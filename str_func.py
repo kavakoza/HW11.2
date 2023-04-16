ae7e2a559a069864f71841910fecb09a3a14699b
Tatyana Kazakova <119956548+kavakoza@users.noreply.github.com>
2023-04-16 22:34:17 +0400

    change str_func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,15 @@
 def upper():
+    """
+    Upper case
+    :return:
+    """
     text = input().upper()
     print(text)
 
+def title():
+    """
+    Title text
+    :return:
+    """
+    text = input().title()
+    print(text)
